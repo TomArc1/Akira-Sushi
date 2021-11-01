d35f83d182fb0abf15097d28d9b6bdb27164ec75
Tomas Arcaria <tarcaria@gmail.com>
2021-11-01 10:57:52 -0300

    se agrega modal y toaster

---
+++ js/modal.js
@@ -0,0 +1,110 @@
+// CONSTRUCCIÓN DE MODAL
+
+const modalContainer = document.getElementById('modal-container');
+const modal = document.getElementById('modal');
+const modalData = document.createElement('div');
+
+
+modalData.innerHTML = `
+    <div class="modal-info">
+        <img src="./assets/images/narumodal.png" class="narumodal">
+        <div class="modal-info__inner">
+            <h3 class="modal-info__inner-h3">Konnichiwa!</h3>
+            <p class="modal-info__inner-p">Suscribite a nuestro newsletter y te contaremos sobre nuestros nuevos lanzamientos y promociones</p>
+            <button id="modal-btn-no" class="modal-btn-no">
+                <img src="./assets/icons/as-cross.svg">
+            </button>
+            <form id="form">
+                <input type="email" class="inputEmail" id="inputEmail" placeholder="Ingresa tu email"> <br>
+                <span id="text-validation" class="text-validation">El email no es válido</span>
+                <button type="submit" id="modal-btn" class="modal-btn" for="form">Suscribirse</button><br>
+            </form>
+
+        </div>
+    </div>
+`;
+
+modal.append(modalData);
+
+// CONSTRUCCIÓN DE TOASTER
+const toaster = document.getElementById('toaster');
+const toasterInner = document.createElement('div');
+
+
+toasterInner.innerHTML = `
+                <div class="toast" id="toast">
+                    <img src="./assets/icons/as-checkmark.svg">
+                    <p>Correo Registrado!</p>
+                </div>
+ `
+toaster.append(toasterInner);
+
+
+
+// APERTURA Y CIERRE DEL MODAL 
+const rechazoBtn = document.getElementById('modal-btn-no');
+const suscribirBtn = document.getElementById('modal-btn');
+
+
+if(sessionStorage.getItem("modalShowFirst") == null){
+    window.addEventListener('load', () => {
+        modalContainer.classList.add('modal-show');
+    }) 
+}
+
+rechazoBtn.addEventListener('click', () => {
+    modalContainer.classList.remove('modal-show');
+    sessionStorage.setItem("modalShowFirst", false);   
+})
+
+
+
+
+// VALIDACION DEL EMAIL
+const form = document.getElementById('form');
+const inputEmail = document.getElementById('inputEmail');
+const textValidation = document.getElementById('text-validation');
+const pattern = / ^[^ ]+@[^ ]+\.[a-z]{2,3}$ / ;
+
+form.addEventListener('submit', e => {
+    // e.preventDefault();
+    let warnings = "";
+    let enter = false ;
+    let regEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ; 
+    console.log(regEx.test(inputEmail.value))
+    if(!regEx.test(inputEmail.value)){
+        textValidation.classList.add('text-validation--show');
+        e.preventDefault()
+    }
+    else{
+        textValidation.classList.remove('text-validation--show');
+        sessionStorage.setItem("modalShowFirst", true);   
+    }
+})
+
+// TOASTER Y LCTMD ALL BOYS
+
+const toast = document.getElementById('toast')
+let getToast = sessionStorage.getItem("modalShowFirst");
+
+
+if(getToast){
+    window.addEventListener('load', () =>{
+        toast.classList.add('toast--show');
+    } )
+}
+
+setTimeout(() =>{
+    toast.classList.remove('toast--show')
+}, 2000 )
+
+
+
+
+
+
+
+
+
+
+
